12b88d2ec39fee981ae956e7ef0a8731ff4f002d
Makalister Mutz Zager <makalistermutzzager@gmail.com>
2026-06-10 13:12:32 -0300

    feat: implementar integração com WhatsApp de suporte
    - Adicionada biblioteca open
    - Implementada abertura automática do WhatsApp
    - Adicionado redirecionamento para atendimento de suporte
    - Configurada mensagem inicial para contato

--- js/chat-bot.js
+++ js/chat-bot.js
@@ -1,4 +1,5 @@
 const readline = require('readline');
+const open = require('open').default;  //pacote responsavel por abrir o whatsapp
 
 const leitor = readline.createInterface({
     input: process.stdin,
@@ -6,6 +7,11 @@ const leitor = readline.createInterface({
 })
 
 const fs = require('fs');
+
+const numeroSuporte = '5527995128081'  //numero que vai ser aberto
+const mensagem = 'Olá, Gostaria de falar com o suporte!'  //mensagem do whatsapp
+
+const linkWhatsapp = `https://wa.me/${numeroSuporte}?text=${encodeURIComponent(mensagem)}`;  //link para abrir o whatsapp
 
 const dados = JSON.parse(
     fs.readFileSync('../json/dados.json', 'utf8')
@@ -39,8 +45,14 @@ function perguntar() {
 
         if (dados.perguntas.dia.some(p => resposta.includes(p))) {  //O método ".includes()" verifica se uma string contém um determinado texto. O método ".some()" percorre o array e retorna true se pelo menos um item atender à condição.
             console.log(`Hoje é: ${duvidas.dia}`);
+
         } else if (dados.perguntas.cidade.some(p => resposta.includes(p))) {
             console.log(`A cidade que você está proucurando é ${duvidas.cidade}`)
+
+        } else if (dados.perguntas.suporte.some(p => resposta.includes(p))) {
+            console.log('Abrindo Whatsapp do suporte...');
+            open(linkWhatsapp);  //resposavel por abrir o whatsapp
+
         } else {
             console.log('Não entendi oque você quis dizer')
         }
@@ -53,6 +65,7 @@ function perguntar() {
                 console.log('Ok obrigado!')
 
                 leitor.close();  //usar dentro do ultimo leitor para não cortar os outros leitores
+
             } else {
                 perguntar();  //retorna para a pergunta
             }
